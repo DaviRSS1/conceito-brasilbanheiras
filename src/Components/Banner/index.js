dd91a2913151192c7d58347ad63ef7e2bb67bc2f
Davi <davireghimsalles@gmail.com>
2024-09-06 14:05:23 -0300

    ''

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Banner from "./images/banner.jpg";
 import styles from "./Banner.module.css";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { GoDotFill } from "react-icons/go";
 
 function Header() {
   return (
     <div className={styles.banners}>
-      <IoIosArrowBack className={`${styles.arrow} ${styles.arrowLeft}`} />
       <img src={Banner} alt="banner" />
-      <IoIosArrowForward className={`${styles.arrow} ${styles.arrowRight}`} />
+      <div className={styles.dots}>
+        <GoDotFill color="#000" />
+        <GoDotFill />
+        <GoDotFill />
+        <GoDotFill />
+        <GoDotFill />
+      </div>
     </div>
   );
 }
